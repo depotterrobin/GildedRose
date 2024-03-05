be28da4b59b793580d44148d5d43d3b63802dd14
Robin de Potter <robin.depotter@yonderland.com>
2024-03-05 11:44:54 +0100

    refactor: Changed if to switch

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -16,19 +16,24 @@ class GildedRose {
 
     public void updateQuality() {
         for (Item item : items) { // Loop through all items
-            // Start quality decrease / increase logic
-            if (!item.getName().equals(AGED_BRIE)
-                    && !item.getName().equals(BACKSTAGE_PASS)) {  // Check type for normal item and quality higher than 0
-                if (item.getQuality() > LOWEST_QUALITY) { // Quality can't go below 0
-                    if (!item.getName().equals(LEGENDARY_ITEM)) {
-                        item.decreaseQuality();
+            switch (item.getName()) {
+                case AGED_BRIE -> {
+                    item.decreaseSellIn();
+
+                    if (item.getQuality() < HIGHEST_QUALITY) { // if quality is below maximum (quality never goes above 50)
+                        item.increaseQuality();
+                    }
+
+                    if (item.getSellIn() < 0) {
+                        if (item.getQuality() < HIGHEST_QUALITY) {
+                            item.increaseQuality();
+                        }
                     }
                 }
-            } else {
-                if (item.getQuality() < HIGHEST_QUALITY) { // if quality is below maximum (quality never goes above 50)
-                    item.increaseQuality();
+                case BACKSTAGE_PASS -> {
+                    if (item.getQuality() < HIGHEST_QUALITY && item.getSellIn() > 0) { // if quality is below maximum (quality never goes above 50)
+                        item.increaseQuality();
 
-                    if (item.getName().equals(BACKSTAGE_PASS)) { // Check type for backstage item
                         if (item.getSellIn() < 11) {
                             if (item.getQuality() < HIGHEST_QUALITY) {
                                 item.increaseQuality();
@@ -40,31 +45,22 @@ class GildedRose {
                                 item.increaseQuality();
                             }
                         }
-                    }
-                }
-            }
-
-            // Start sellIn decrease logic
-            if (!item.getName().equals(LEGENDARY_ITEM)) { // if legendary item, then DON'T decrease sellIn (they don't have a sellIn)
-                item.decreaseSellIn();
-            }
-
-            // sellIn exceeded below 0 logic
-            if (item.getSellIn() < 0) {
-                if (!item.getName().equals(AGED_BRIE)) {
-                    if (!item.getName().equals(BACKSTAGE_PASS)) {
-                        if (item.getQuality() > LOWEST_QUALITY) {
-                            if (!item.getName().equals(LEGENDARY_ITEM)) { // Quality of legendary item never changes
-                                item.decreaseQuality();
-                            }
-                        }
-                    } else { // Backstage passes quality logic
+                    } else {
                         item.setQualityToZero();
                     }
-                } else { // Aged Brie logic quality logic
-                    if (item.getQuality() < HIGHEST_QUALITY) {
-                        item.increaseQuality();
+
+                    item.decreaseSellIn();
+                }
+                case LEGENDARY_ITEM -> {
+                }
+                default -> {
+                    if (item.getQuality() > LOWEST_QUALITY) { // Quality can't go below 0
+                        item.decreaseQuality();
                     }
+                    if (item.getSellIn() < 0 && item.getQuality() > LOWEST_QUALITY) {
+                        item.decreaseQuality();
+                    }
+                    item.decreaseSellIn();
                 }
             }
         }
