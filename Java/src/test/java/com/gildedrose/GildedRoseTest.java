c559fc12d3f6a5ab44118ae1af4fc8c5fa4f0460
Robin de Potter <robin.depotter@yonderland.com>
2024-03-07 09:51:06 +0100

    test: added parameterized tests

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,8 +1,12 @@
 package com.gildedrose;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -13,21 +17,35 @@ class GildedRoseTest {
         app.updateQuality();
         assertEquals(expected.sellIn, items.get(0).sellIn);
         assertEquals(expected.quality, items.get(0).quality);
+        assertEquals(expected.name, items.get(0).name);
     }
 
-    @Test
-    void given_normalItem_when_endOfDay_then_decreaseQualityAndSellInBy1() {
-        verifyItem(
-            new Item("Normal Item", 10, 25),
-            new Item("Normal Item", 9, 24)
+    private static Stream<Arguments> given_normalItem_when_endOfDay_then_decreaseQualityAndSellInBy1() {
+        return Stream.of(
+            Arguments.of(new Item("Normal Item", 10, 25), new Item("Normal Item", 9, 24)),
+            Arguments.of(new Item("Normal Item", 1, 1), new Item("Normal Item", 0, 0)),
+            Arguments.of(new Item("Normal Item", 0, 50), new Item("Normal Item", -1, 49))
         );
+    }
+
+    private static Stream<Arguments> given_agedBrieItem_when_endOfDay_then_qualityIncreases() {
+        return Stream.of(
+            Arguments.of(new Item("Aged Brie", 10, 4), new Item("Aged Brie", 9, 5)),
+            Arguments.of(new Item("Aged Brie", 1, 4), new Item("Aged Brie", 0, 5)),
+            Arguments.of(new Item("Aged Brie", 0, 4), new Item("Aged Brie", -1, 5))
+            );
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    void given_normalItem_when_endOfDay_then_decreaseQualityAndSellInBy1(Item input, Item expected) {
+        verifyItem(input, expected);
     }
 
     @Test
     void given_itemWithLowestQuality_when_endOfDay_then_qualityNotChangedAndSellInDecreases() {
         verifyItem(
-            new Item("Normal Item", 10, 0),
-            new Item("Normal Item", 9, 0)
+            new Item("Normal Item", 0, 0), new Item("Normal Item", -1, 0)
         );
     }
 
@@ -39,19 +57,17 @@ class GildedRoseTest {
         );
     }
 
-    @Test
-    void given_agedBrieItem_when_endOfDay_then_qualityIncreases() {
-        verifyItem(
-            new Item("Aged Brie", 20, 4),
-            new Item("Aged Brie", 19, 5)
-        );
+    @ParameterizedTest
+    @MethodSource
+    void given_agedBrieItem_when_endOfDay_then_qualityIncreases(Item input, Item expected) {
+        verifyItem(input, expected);
     }
 
     @Test
     void given_agedBrieItemWithNegativeSellIn_when_endOfDay_then_qualityIncreasesBy2() {
         verifyItem(
-            new Item("Aged Brie", -5, 4),
-            new Item("Aged Brie", -6, 6)
+            new Item("Aged Brie", -1, 4),
+            new Item("Aged Brie", -2, 6)
         );
     }
 
@@ -72,6 +88,14 @@ class GildedRoseTest {
     }
 
     @Test
+    void given_backstagePassItemWithSellInHigherThan10_when_endOfDay_then_qualityIncreasesBy1() {
+        verifyItem(
+            new Item("Backstage passes to a TAFKAL80ETC concert", 11, 30),
+            new Item("Backstage passes to a TAFKAL80ETC concert", 10, 31)
+        );
+    }
+
+    @Test
     void given_backstagePassItemWithSellInBetween5And11_when_endOfDay_then_qualityIncreasesBy2() {
         verifyItem(
             new Item("Backstage passes to a TAFKAL80ETC concert", 6, 30),
@@ -82,8 +106,8 @@ class GildedRoseTest {
     @Test
     void given_backstagePassItemWithSellInBetween0And5_when_endOfDay_then_qualityIncreasesBy3() {
         verifyItem(
-            new Item("Backstage passes to a TAFKAL80ETC concert", 2, 30),
-            new Item("Backstage passes to a TAFKAL80ETC concert", 1, 33)
+            new Item("Backstage passes to a TAFKAL80ETC concert", 1, 30),
+            new Item("Backstage passes to a TAFKAL80ETC concert", 0, 33)
         );
     }
 
@@ -103,4 +127,11 @@ class GildedRoseTest {
         );
     }
 
+    @Test
+    void given_backstagePassItemWithMaximumQuality_when_endOfDay_then_qualityNotChanged() {
+        verifyItem(
+            new Item("Backstage passes to a TAFKAL80ETC concert", 11, 50),
+            new Item("Backstage passes to a TAFKAL80ETC concert", 10, 50)
+        );
+    }
 }
